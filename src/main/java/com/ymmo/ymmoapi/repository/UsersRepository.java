3bfce5dce6c4e805e3e4b0d14d9b8d4e1e109bae
cnoah <noah.charrinbourrat@ynov.com>
2026-05-29 18:41:54 +0200

    feat: users repository added for user data manipulation

---
+++ src/main/java/com/ymmo/ymmoapi/repository/UsersRepository.java
@@ -0,0 +1,12 @@
+package com.ymmo.ymmoapi.repository;
+
+import com.ymmo.ymmoapi.model.Users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    Users findByEmail(String email);
+    Users findById(int id);
+}
