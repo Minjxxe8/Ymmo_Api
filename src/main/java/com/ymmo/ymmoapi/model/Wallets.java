d132f905499e560847b2d5b4aa35452d5429b6f7
cnoah <noah.charrinbourrat@ynov.com>
2026-05-29 17:44:55 +0200

    feat: generated value added to id

--- src/main/java/com/ymmo/ymmoapi/model/Wallets.java
+++ src/main/java/com/ymmo/ymmoapi/model/Wallets.java
@@ -1,12 +1,12 @@
 package com.ymmo.ymmoapi.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
 
 @Entity(name = "wallets")
 public class Wallets {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
