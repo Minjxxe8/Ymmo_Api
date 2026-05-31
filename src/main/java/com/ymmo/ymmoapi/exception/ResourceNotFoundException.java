c41f05714166275571ff68dd662f7dc6d002ce8c
cnoah <noah.charrinbourrat@ynov.com>
2026-05-31 16:41:52 +0200

    feat: exception added for not found content

---
+++ src/main/java/com/ymmo/ymmoapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ymmo.ymmoapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NO_CONTENT)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
