52ebed69148a4a69521dba27f20f974240e9dbef
cnoah <noah.charrinbourrat@ynov.com>
2026-06-08 23:54:14 +0200

    feat: Response entity type have been changed to accepts any type as a return

--- src/main/java/com/ymmo/ymmoapi/controller/AuthController.java
+++ src/main/java/com/ymmo/ymmoapi/controller/AuthController.java
@@ -39,11 +39,11 @@ public class AuthController {
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<UserAuthDto.AuthResponse> Refresh(@RequestBody UserAuthDto.RefreshRequest refreshRequest) {
+    public ResponseEntity<?> Refresh(@RequestBody UserAuthDto.RefreshRequest refreshRequest) {
         try {
             return ResponseEntity.ok(authService.refresh(refreshRequest));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
         }
     }
 
