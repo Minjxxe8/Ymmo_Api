d4a1a153089ec9068060afa61a30fdb91b17261e
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-06-09 17:02:20 +0200

    feat: pre authorize added and cross origin for letting the front end access to it

--- src/main/java/com/ymmo/ymmoapi/controller/UserController.java
+++ src/main/java/com/ymmo/ymmoapi/controller/UserController.java
@@ -6,12 +6,14 @@ import com.ymmo.ymmoapi.repository.UsersRepository;
 import com.ymmo.ymmoapi.service.PasswordService;
 import com.ymmo.ymmoapi.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class UserController {
 
     private final UserService userService;
@@ -35,16 +37,20 @@ public class UserController {
     }
 
     @PostMapping("/users")
+    @PreAuthorize("hasRole('admin')")
     public ResponseEntity<Users> createUser(@RequestBody UserCreationDto user) {
         return ResponseEntity.ok(userService.createUser(user));
     }
 
+
     @PatchMapping("/users/{id}")
+    @PreAuthorize("hasRole('admin')")
     public ResponseEntity<Users> updateUser(@PathVariable Long id, @RequestBody Users user) {
         return ResponseEntity.ok(userService.updateUser(id, user));
     }
 
     @DeleteMapping("/users/{id}")
+    @PreAuthorize("hasRole('admin')")
     public ResponseEntity<Users> deleteUser(@PathVariable Long id) {
         return ResponseEntity.ok(userService.deleteUser(id));
     }
