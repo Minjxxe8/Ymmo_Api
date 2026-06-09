cbf5b08b1109c94968070b7c1701aacf0381ca45
cnoah <noah.charrinbourrat@ynov.com>
2026-06-10 00:27:45 +0200

    feat: ResourceNotFoundException has been added as child class frrom ResponseException for easier error managment (mainly for getting http code)

--- src/main/java/com/ymmo/ymmoapi/exception/ResourceNotFoundException.java
+++ src/main/java/com/ymmo/ymmoapi/exception/ResourceNotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NO_CONTENT)
-public class ResourceNotFoundException extends RuntimeException {
+public class ResourceNotFoundException extends ResponseException {
     public ResourceNotFoundException(String message) {
-        super(message);
+        super(message, 204);
     }
 }
