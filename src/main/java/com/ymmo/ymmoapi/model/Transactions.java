d1fd89a515b8586f114d90d3e9d5de1d5d8591d1
cnoah <noah.charrinbourrat@ynov.com>
2026-06-11 00:22:56 +0200

    feat: refactor Transactions model to use Users and Properties entities

--- src/main/java/com/ymmo/ymmoapi/model/Transactions.java
+++ src/main/java/com/ymmo/ymmoapi/model/Transactions.java
@@ -14,15 +14,76 @@ public class Transactions {
     @Column(name = "description")
     private String description;
 
-    @Column(name = "user_id")
-    private int userId;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private Users users;
 
     @Column(name = "amount")
     private double amount;
 
-    @Column(name = "property_id")
-    private int propertyId;
+    @OneToOne
+    @JoinColumn(name = "property_id")
+    private Properties properties;
 
-    @Column(name = "created_at")
+    @Column(name = "created_at", updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private Timestamp createdAt;
+
+    public Transactions(String description, Users users, double amount, Properties properties) {
+        this.description = description;
+        this.users = users;
+        this.amount = amount;
+        this.properties = properties;
+    }
+
+    public Transactions() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
 }
