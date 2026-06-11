a082f145564b7e9384ac70a8c4382a5869e9c58c
cnoah <noah.charrinbourrat@ynov.com>
2026-06-12 00:27:52 +0200

    feat(propertyTypes): getters for property types have been added

--- src/main/java/com/ymmo/ymmoapi/model/PropertyTypes.java
+++ src/main/java/com/ymmo/ymmoapi/model/PropertyTypes.java
@@ -11,4 +11,12 @@ public class PropertyTypes {
 
     @Column(name = "name")
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
