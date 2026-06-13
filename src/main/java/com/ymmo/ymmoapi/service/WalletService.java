774d6552bc4c7b66639a0bd7b67d343016d02127
cnoah <noah.charrinbourrat@ynov.com>
2026-06-13 16:31:58 +0200

    feat(wallet): wallet service for getting a wallet, creating one, adding or removing balance on an existing one

---
+++ src/main/java/com/ymmo/ymmoapi/service/WalletService.java
@@ -0,0 +1,54 @@
+package com.ymmo.ymmoapi.service;
+
+import com.ymmo.ymmoapi.dto.WalletsDto;
+import com.ymmo.ymmoapi.exception.ResourceNotFoundException;
+import com.ymmo.ymmoapi.exception.ResponseException;
+import com.ymmo.ymmoapi.model.Users;
+import com.ymmo.ymmoapi.model.Wallets;
+import com.ymmo.ymmoapi.repository.UsersRepository;
+import com.ymmo.ymmoapi.repository.WalletRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WalletService {
+    private final WalletRepository walletRepository;
+    private final UsersRepository usersRepository;
+
+    public WalletService(WalletRepository walletRepository, UsersRepository usersRepository) {
+        this.walletRepository = walletRepository;
+        this.usersRepository = usersRepository;
+    }
+
+    public WalletsDto.WalletResponse createWallet(Users user) {
+        Wallets newWallet = walletRepository.save(new Wallets(
+                user,
+                0
+        ));
+        return new WalletsDto.WalletResponse(newWallet.getId(), newWallet.getBalance());
+    }
+
+    public WalletsDto.WalletResponse getUserWallet(String email) {
+        Wallets userWaller = getUserWalletByEmail(email);
+        return new WalletsDto.WalletResponse(userWaller.getId(), userWaller.getBalance());
+    }
+
+    public WalletsDto.WalletResponse addBalance(String email, int balance) {
+        Wallets userWaller = getUserWalletByEmail(email);
+        userWaller.addBalance(balance);
+        Wallets newValue = walletRepository.save(userWaller);
+        return new WalletsDto.WalletResponse(newValue.getId(), newValue.getBalance());
+    }
+
+    public WalletsDto.WalletResponse removeBalance(String email, int balance) {
+        Wallets userWaller = getUserWalletByEmail(email);
+        userWaller.removeBalance(balance);
+        Wallets newValue = walletRepository.save(userWaller);
+        return new WalletsDto.WalletResponse(newValue.getId(), newValue.getBalance());
+    }
+
+    private Wallets getUserWalletByEmail(String email) {
+        return walletRepository.findByUser(
+                usersRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("User not found with the email : " + email))
+        );
+    }
+}
