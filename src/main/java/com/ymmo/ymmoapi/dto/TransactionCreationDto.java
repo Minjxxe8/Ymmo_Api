801481ec3ae5cd04ccace8d75e846f3e945bb57c
cnoah <noah.charrinbourrat@ynov.com>
2026-06-13 17:50:55 +0200

    feat(transaction): unused values have been removed

--- src/main/java/com/ymmo/ymmoapi/dto/TransactionCreationDto.java
+++ src/main/java/com/ymmo/ymmoapi/dto/TransactionCreationDto.java
@@ -3,22 +3,10 @@ package com.ymmo.ymmoapi.dto;
 public class TransactionCreationDto {
     private String description;
 
-    private int userId;
-
-    private double amount;
-
     private int propertyId;
 
     public String getDescription() {
         return description;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public double getAmount() {
-        return amount;
     }
 
     public int getPropertyId() {
