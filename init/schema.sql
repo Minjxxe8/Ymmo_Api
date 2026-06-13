639d7bc8d5a4388306423068689e3bd2b9915dcc
cnoah <noah.charrinbourrat@ynov.com>
2026-06-13 22:50:09 +0200

    feat(schema): new table for favorites added

--- init/schema.sql
+++ init/schema.sql
@@ -62,3 +62,13 @@ CREATE TABLE transactions
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (property_id) REFERENCES properties (id) ON DELETE CASCADE
 );
+
+CREATE TABLE favorites
+(
+    id          SERIAL PRIMARY KEY,
+    user_id     INTEGER NOT NULL,
+    property_id INTEGER NOT NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (property_id) REFERENCES properties (id) ON DELETE CASCADE
+)
